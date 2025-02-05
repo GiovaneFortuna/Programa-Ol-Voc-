359d69dbd32da136230ba99f0017eb51dae6e93a
Giovane Fortuna <Fortuna_giovane47@hotmail.com>
2025-02-04 23:54:27 -0300

    adicionando um comentario

--- script.js
+++ script.js
@@ -11,3 +11,14 @@ function mostrarNome(){
 
 const btMostrar = document.getElementById("btMostrar");
 btMostrar.addEventListener("click", mostrarNome);
+
+/*
+
+Os comandos que pertencem a uma função devem ser
+delimitados pelas chaves { }. A função que obtém o conteúdo 
+do campo nome de um formulário e a exibe precedida pela palavra Olá
+em um parágrafo com id="resposta" é a seguinte:
+
+
+
+*/
